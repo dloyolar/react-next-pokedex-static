b552d8753d023f4a1766bb9498992b26cbe7ff43
dloyolar <dieegolr@gmail.com>
2022-03-05 16:21:22 -0300

    [ADD] ✨get pokemons from api with StaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
 import type { NextPage } from 'next';
-import { Layout } from '../components/layouts';
+import { GetStaticProps } from 'next';
 
-const HomePage: NextPage = () => {
+import { Layout } from '../components/layouts';
+import pokeApi from '../api/pokeApi';
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get('pokemon?limit=151');
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+};
+
+const HomePage: NextPage = (props) => {
+  console.log(props);
   return (
     <Layout title="Pokemon List">
-      <h1>Init</h1>
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+      </ul>
     </Layout>
   );
 };
